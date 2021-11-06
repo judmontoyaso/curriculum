1c53793376234e52a484919aaba8a21f2b1c37aa
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-06 16:40:06 -0500

    Corregir posicion themeswitch

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,22 +1,24 @@
-import Link from "next/link"
-import React from "react"
+import Link from "next/link";
+import React from "react";
 import ThemeSwitch from "./ThemeSwitch";
 
 const Navigation = () => {
-    return (
-        <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
-            <ThemeSwitch></ThemeSwitch>
-            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
-                <Link href="/">
-                    <a
-                        className={"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"}
-                    >
-                        Juan David Montoya
-                    </a>
-                </Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
+      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+        <Link href="/">
+          <a
+            className={
+              "font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
+            }
+          >
+            Juan David Montoya
+          </a>
+        </Link>
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
