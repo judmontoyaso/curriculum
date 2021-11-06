ddc1ce5d9b35c420efd8e22a1fd326e8cc395bcc
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-06 16:30:58 -0500

    agregar navbar, footer, foto y darkmodesupport

---
+++ components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import Image from "next/image"
+import profile from "/public/profile.jpg"
+
+const About = () => {
+    return (
+        <div className="container px-4 mx-auto">
+            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+                <div className="lg:px-4 lg:mt-12 ">
+                    <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+                        Hey there,
+                    </h1>
+                    <div className="mt-6 text-gray-800 dark:text-white">
+                        <p className="mb-4">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a
+                            type specimen book.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+                    <Image
+                        src={profile}
+                        alt="Profile"
+                        priority={true}
+                        className="rounded-full"
+                        width={200}
+                        height={250}
+                        placeholder="blur"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider attribute="class" enableSystem={false}>
+      <Navigation />
+      <Component {...pageProps} />
+      <Footer />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
