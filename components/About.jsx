bfc583f014280523427cc9db5f1ffd2a6ec4ef89
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-08 10:20:21 -0500

    Agregar estilo a boton dark con translate

--- components/About.jsx
+++ components/About.jsx
@@ -8,16 +8,20 @@ const About = () => {
   const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
   return (
     <div className="container px-4 mx-auto">
-
       <RoughNotationGroup show={true}>
         <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
           <div className="lg:px-4 lg:mt-12 ">
             <h1 className="text-xl font-bold text-gray-900 lg:text-3xl dark:text-white">
               Hola!, mi nombre es{" "}
-              <RainbowHighlight color={colors[3]} type={"underline"}>
+              <RoughNotation
+                color={colors[3]}
+                type="underline"
+                order="2"
+                padding={[0, 1]}
+              >
                 {" "}
                 Juan David{" "}
-              </RainbowHighlight>
+              </RoughNotation>
             </h1>
             <div className="mt-6 text-gray-800 dark:text-white">
               <p className="mb-4">
--- components/ThemeSwitch.jsx
+++ components/ThemeSwitch.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
@@ -12,36 +14,33 @@ const ThemeSwitch = () => {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="w-9 h-9 p-1 ml-1 mr-1 rounded-lg
-      dark:border-yellow-600 border-blue-500 sm:ml-12 border-2"
+      className="w-16 h-8 pr-1 pl-1 ml-1 mr-1 rounded-l-full rounded-r-full 
+      dark:border-yellow-600 border-blue-500 sm:ml-12 border-2 dark:transform justify-around dark:bg-gray-400 bg-blue-300 focus:outline outline-none"
       onClick={() =>
         setTheme(
           theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
         )
       }
     >
+      <div className="rounded-full h-6 w-6 bg-gray-200 dark:transform dark:translate-x-7 dark:absolute duration-700 absolute"></div>
       {mounted && (theme === "dark" || resolvedTheme === "dark") ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="text-blue-500 dark:text-yellow-500 "
-        >
-          <path
-            fillRule="evenodd"
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            clipRule="evenodd"
+        <>
+          <FontAwesomeIcon icon={faSun} size="" className="text-yellow-400" />
+          <FontAwesomeIcon
+            icon={faMoon}
+            size=""
+            className="text-blue-500 ml-4"
           />
-        </svg>
+        </>
       ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="text-blue-500 dark:text-yellow-500"
-        >
-          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-        </svg>
+        <div className="">
+          <FontAwesomeIcon icon={faSun} size="" className="text-yellow-400" />
+          <FontAwesomeIcon
+            icon={faMoon}
+            size=""
+            className="text-blue-700  ml-4"
+          />
+        </div>
       )}
     </button>
   );
