9260e35383ce2c68e0df08843debf4a01cb291f8
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-07 22:18:30 -0500

    Agregar transition dark mode

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -14,17 +14,17 @@ const Navigation = () => {
     setActive(!active);
   };
   return (
-    <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
+    <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black  transition-color duration-1000">
       <ThemeSwitch />
 
       <button
-        className="float-right pr-3 pl-3 pt-1 dark:hover:text-blue-400 hover:text-green-500 rounded-sm lg:hidden text-black ml-auto outline-none justify-end dark:text-white"
+        className="float-right pr-3 pl-3 pt-1 dark:hover:text-blue-400 hover:text-green-500 rounded-sm lg:hidden text-black ml-auto outline-none justify-end dark:text-white  hover:text-xl"
         onClick={handleClick}
       >
         {!active ? (
-          <FontAwesomeIcon icon={faHamburger} size="2x" />
+          <FontAwesomeIcon icon={faHamburger} size="lg" />
         ) : (
-          <FontAwesomeIcon icon={faHotdog} size="2x" />
+          <FontAwesomeIcon icon={faHotdog} size="lg" />
         )}
       </button>
 
@@ -33,19 +33,19 @@ const Navigation = () => {
           active ? "" : "hidden"
         }   w-full lg:inline-flex lg:flex-grow lg:w-auto lg:float-right`}
       >
-        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:mr-20 lg:w-auto w-full lg:items-center items-end  flex flex-col lg:h-auto">
+        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:mr-20 lg:w-auto w-full lg:items-center items-end  flex flex-col lg:h-auto  lg:sticky absolute dark:bg-black dark:bg-opacity-20 bg-gray-200 bg-opacity-20  transition-color duration-1000">
           <Link href="/">
-            <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400 dark:border-black border-b-2 border-white hover:text-green-400 hover:border-green-500 dark:text-white">
+            <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400  hover:border-b-2 hover:text-green-400 hover:border-green-500 dark:text-white  lg:mt-0 mt-4">
               <span className="">Curriculum</span>
             </a>
           </Link>
           <Link href="/">
-            <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold  items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400 dark:border-black border-b-2 border-white hover:text-green-400 hover:border-green-500 dark:text-white">
+            <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold  items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400  hover:border-b-2 hover:text-green-400 hover:border-green-500 dark:text-white">
               <span className="">Proyectos</span>
             </a>
           </Link>
           <Link href="/">
-            <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold  items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400 dark:border-black border-b-2   border-white hover:text-green-400 hover:border-green-500 dark:text-white">
+            <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold  items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400  hover:border-b-2   hover:text-green-400 hover:border-green-500 dark:text-white">
               <span className="">Contacto</span>
             </a>
           </Link>
--- components/ThemeSwitch.jsx
+++ components/ThemeSwitch.jsx
@@ -25,7 +25,7 @@ const ThemeSwitch = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          className="text-blue-500 dark:text-yellow-500"
+          className="text-blue-500 dark:text-yellow-500 "
         >
           <path
             fillRule="evenodd"
