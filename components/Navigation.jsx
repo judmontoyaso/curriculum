7417b584a1b65aa6f2dc32a5d12c76a0d9bb7062
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-07 22:35:04 -0500

    Agregar transition dark mode

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -33,7 +33,7 @@ const Navigation = () => {
           active ? "" : "hidden"
         }   w-full lg:inline-flex lg:flex-grow lg:w-auto lg:float-right`}
       >
-        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:mr-20 lg:w-auto w-full lg:items-center items-end  flex flex-col lg:h-auto  lg:sticky absolute dark:bg-black dark:bg-opacity-20 bg-gray-200 bg-opacity-20  transition-color duration-1000">
+        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:mr-20 lg:w-auto w-full lg:items-center items-end  flex flex-col lg:h-auto  lg:sticky absolute dark:bg-black dark:bg-opacity-40 bg-white bg-opacity-40  transition-color duration-1000 ">
           <Link href="/">
             <a className="lg:mr-8 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-2xl text-black font-bold items-center justify-center dark:hover:text-blue-300 dark:hover:border-blue-400  hover:border-b-2 hover:text-green-400 hover:border-green-500 dark:text-white  lg:mt-0 mt-4">
               <span className="">Curriculum</span>
