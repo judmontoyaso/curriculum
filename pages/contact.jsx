d6a4b724123cbac4e1d422de5b13b8cf4a173a77
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-09 17:24:02 -0500

    corregir hook y clases

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -107,7 +107,7 @@ const Contact = () => {
               ) : null}
             </div>
             <div className="col-span-2">
-              <label className="text-gray-700" for="name">
+              <label className="text-gray-700" htmlFor="name">
                 <textarea
                   className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   id="mensaje"
