5199d834fd49a195b4593cc15d9c17e7288fd91c
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-10 13:56:40 -0500

    agregar mensaje en paginas sin contenido

--- pages/curriculum.jsx
+++ pages/curriculum.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const curriculum = () => {
-    return (
-        <div className="">
-            Aca va el curriculum
+  return (
+    <div className="">
+      <main class="bg-white relative overflow-hidden h-screen">
+  
+        <div class="container mx-auto h-screen pt-32 md:pt-0 px-6 z-10 flex items-center justify-between">
+          <div class="container mx-auto px-6 flex flex-col-reverse lg:flex-row justify-between items-center relative">
+            <div class="w-full mb-16 md:mb-8 text-center lg:text-left">
+              <h1 class="font-light font-sans text-center lg:text-left text-5xl lg:text-8xl mt-12 md:mt-0 text-gray-700">
+              Lo siento, sitio en construcción
+              </h1>
+            </div>
+          </div>
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
 
-export default curriculum
+export default curriculum;
--- pages/project.jsx
+++ pages/project.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const project = () => {
-  return <div>aca va el portafolio</div>;
+  return <div>   <div className="">
+  <main class="bg-white relative overflow-hidden h-screen">
+ 
+    <div class="container mx-auto h-screen pt-32 md:pt-0 px-6 z-10 flex items-center justify-between">
+      <div class="container mx-auto px-6 flex flex-col-reverse lg:flex-row justify-between items-center relative">
+        <div class="w-full mb-16 md:mb-8 text-center lg:text-left">
+          <h1 class="font-light font-sans text-center lg:text-left text-5xl lg:text-8xl mt-12 md:mt-0 text-gray-700">
+          Lo siento, sitio en construcción
+          </h1>
+        </div>
+      </div>
+    </div>
+  </main>
+</div></div>;
 };
 
 export default project;
