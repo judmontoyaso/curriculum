d6ebece64b9a4fade8325aa64b7a22127ba681e9
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-10 07:32:12 -0500

    Agregar numero aleatroio para identificacion de email

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,30 +1,41 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import emailjs, {send} from 'emailjs-com'
-import { useState } from "react";
+import emailjs, { send } from "emailjs-com";
+import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 
 const Contact = () => {
-  const [contactNumber, setContactNumber] = useState("000000");
+  
+  
+  const numStr = "000000" + ((Math.random() * 1000000) | 0);
+  const numero = numStr.substring(numStr.length - 6);
+  const [contactNumber, setContactNumber] = useState(numero);
 
-  const generateContactNumber = () => {
-    const numStr = "000000" + ((Math.random() * 1000000) | 0);
-    setContactNumber(numStr.substring(numStr.length - 6));
-  };
+
+  
+ 
+
 
   function SendEmail(object) {
-    emailjs.send("formulario_contacto_cv",
-    "template_5243rb1",
-    object,
-    "user_DD9uUMu5CFcOAGZ2Ah34m")
-        .then((result) => {
-            console.log(result.text)
-            Swal.fire("Enviado!", "El mensaje se ha enviado", "success");
-        }, (error) => {
-            console.log(error.text)
-        })
-}
+    emailjs
+      .send(
+        "formulario_contacto_cv",
+        "template_5243rb1",
+        object,
+        "user_DD9uUMu5CFcOAGZ2Ah34m"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire("Enviado!", "El mensaje se ha enviado", "success");
+          formik.resetForm();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 
   const formik = useFormik({
     initialValues: {
@@ -40,15 +51,14 @@ const Contact = () => {
       mensaje: Yup.string().required("El mensaje  es obligatorio"),
     }),
 
-    //Onsubmit create predio
     onSubmit: async (valores) => {
-        generateContactNumber();
       const { nombre, email, mensaje, numero } = valores;
 
-      
       try {
-      SendEmail(valores);
-      console.log(valores);
+        SendEmail(valores);
+        console.log(numero);
+
+        console.log(valores);
       } catch (error) {
         console.log(error);
       }
@@ -65,7 +75,7 @@ const Contact = () => {
       >
         <div className="w-full max-w-2xl px-5 py-10 m-auto mt-10 bg-white rounded-lg shadow dark:bg-gray-800">
           <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
-            Contact us !
+            Contactame!
           </div>
           <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
             <div className="col-span-2 lg:col-span-1">
@@ -74,7 +84,7 @@ const Contact = () => {
                   type="text"
                   id="nombre"
                   className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Name"
+                  placeholder="Nombre"
                   value={formik.values.nombre}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
@@ -111,7 +121,7 @@ const Contact = () => {
                 <textarea
                   className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   id="mensaje"
-                  placeholder="Enter your comment"
+                  placeholder="Ingresa tu comentario"
                   name="mensaje"
                   rows="5"
                   cols="40"
@@ -138,7 +148,7 @@ const Contact = () => {
                 type="submit"
                 className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
-                Send
+                Enviar
               </button>
             </div>
           </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
+import Contact from "./contact";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
