95ec5f3eddfee6c3eb2495a1810a556fe0738731
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-11-10 14:01:59 -0500

    agregar corregir estilos

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -4,12 +4,12 @@ const blog = () => {
     return (
         <div>
                <div className="">
-      <main class="bg-white relative overflow-hidden h-screen">
+      <main className="bg-white relative overflow-hidden h-screen">
 
-        <div class="container mx-auto h-screen pt-32 md:pt-0 px-6 z-10 flex items-center justify-between">
-          <div class="container mx-auto px-6 flex flex-col-reverse lg:flex-row justify-between items-center relative">
-            <div class="w-full mb-16 md:mb-8 text-center lg:text-left">
-              <h1 class="font-light font-sans text-center lg:text-left text-5xl lg:text-8xl mt-12 md:mt-0 text-gray-700">
+        <div className="container mx-auto h-screen pt-32 md:pt-0 px-6 z-10 flex items-center justify-between">
+          <div className="container mx-auto px-6 flex flex-col-reverse lg:flex-row justify-between items-center relative">
+            <div className="w-full mb-16 md:mb-8 text-center lg:text-left">
+              <h1 className="font-light font-sans text-center lg:text-left text-5xl lg:text-8xl mt-12 md:mt-0 text-gray-700">
                 Lo siento, sitio en construcción
               </h1>
             </div>
