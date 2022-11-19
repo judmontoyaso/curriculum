f6e96256d73a3cc3cd5993efdf2be61d32606757
Juan David Montoya <82336705+judmontoyaso@users.noreply.github.com>
2022-11-19 00:17:20 +0000

    Agregar tostify notificacion para formulario contacto

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-
+import { ToastContainer, toast, Flip } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import emailjs, { send } from "emailjs-com";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Swal from "sweetalert2";
 import { Particle } from "../components/Particle";
+import { useTheme } from "next-themes";
 
 
 // export async function getServerSideProps(context) {
@@ -22,12 +24,18 @@ import { Particle } from "../components/Particle";
 //   }
 
 const Contact = () => {
-
+  const {resolvedTheme } = useTheme();
   const numStr = "000000" + ((Math.random() * 1000000) | 0);
   const numero = numStr.substring(numStr.length - 6);
   const [contactNumber, setContactNumber] = useState(numero);
+  const [loading, setLoading] = useState(false)
+ 
+
+console.log(loading)
+
 
   function SendEmail(object) {
+ 
     emailjs
       .send(
         "formulario_contacto_cv",
@@ -38,23 +46,52 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+         
+          toast.success('Gracias por escribirme, me pondré en contácto pronto!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: resolvedTheme === "dark" ? "dark" : "light",
+            transition: Flip
 
-          Swal.fire(
-            "Enviado!",
-            "Gracias por escribirme, me pondré en contácto pronto",
-            "success"
-          );
+            });setLoading(false);
+
+           
+           
+          // Swal.fire(
+          //   "Enviado!",
+          //   "Gracias por escribirme, me pondré en contácto pronto",
+          //   "success"
+          // );
           formik.resetForm();
         },
         (error) => {
-          Swal.fire(
-            "Error",
-            "Ocurrio un error, por favor intente más tarde",
-            "error"
-          );
+
+
+          toast.error('Ocurrio un error, por favor intente más tarde', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: resolvedTheme === "dark" ? "dark" : "light",
+            transition: Flip
+
+            });
+          // Swal.fire(
+          //   "Error",
+          //   "Ocurrio un error, por favor intente más tarde",
+          //   "error"
+          // );
           console.log(error.text);
         }
-      );
+      ); 
   }
 
   const formik = useFormik({
@@ -72,15 +109,22 @@ const Contact = () => {
     }),
 
     onSubmit: async (valores) => {
+      setLoading(true)
+      console.log(loading);
+
       const { nombre, email, mensaje, numero } = valores;
 
       try {
         SendEmail(valores);
+      
       } catch (error) {
         console.log(error);
+     
+
       }
 
-      console.log("enviando");
+      console.log(loading);
+
     },
   });
   return (
@@ -95,6 +139,7 @@ const Contact = () => {
           className="flex w-3/4 max-w-sm space-x-3 lg:w-full lg:max-w-max"
           onSubmit={formik.handleSubmit}
         >
+           <ToastContainer/>
           <div className="w-full max-w-full lg:max-w-full px-5 py-10 m-auto mt-10 bg-white rounded-xl shadow-2xl dark:bg-gray-800 transition-colors duration-1000 dark:shadow-inner">
             <div className="mb-10 text-3xl font-light text-center text-gray-800 dark:text-white">
               <span className="font-semibold ">¡ Contáctame !</span>
@@ -166,11 +211,16 @@ const Contact = () => {
                 value={formik.values.numero}
               />
               <div className="col-span-2 text-right">
+                
                 <button
                   type="submit"
                   className="py-2 px-4  bg-indigo-600 hover:bg-green-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-xl "
                 >
-                  Enviar
+              <span className="h-screen w-full flex justify-center items-center">
+      <span className="animate-spin relative flex h-10 w-10 rounded-sm bg-purple-400 opacity-75"></span>
+    </span>
+    
+                  {!loading ? "Enviar" : "Enviando"}
                 </button>
               </div>
             </div>
